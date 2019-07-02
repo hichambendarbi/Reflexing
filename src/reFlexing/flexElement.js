a435ee3d1482c2fc07ff8c5a7cfa8173b56ea3ac
khmmamed <khmammed@gmail.com>
2019-07-02 22:38:38 +0100

    flexElement
    flexElement

--- src/reFlexing/flexElement.js
+++ src/reFlexing/flexElement.js
@@ -1,9 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-
 const styledElement = styled.div`
-  flex : 1 100%;
-`
+  flex: 1 100%;
+`;
 
-export const FlexElement = ({children})=><styledElement>Flex Element</styledElement>
+export const FlexElement = ({
+  children,
+  grow,
+  shrink,
+  basis,
+  order,
+  flex,
+  self
+}) => <styledElement>Flex Element</styledElement>;
