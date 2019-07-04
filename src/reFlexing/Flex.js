87fb6252e60282ba3f821ac8f0dce8075172d698
khmmamed <khmammed@gmail.com>
2019-07-04 14:43:39 +0100

    flex styling

--- src/reFlexing/Flex.js
+++ src/reFlexing/Flex.js
@@ -3,10 +3,18 @@ import styled from "styled-components";
 
 const BoxContainer = styled.div`
   display: flex;
+  flex-flow: ${props => (props.flow ? props.flow : "")};
   flex-direction: ${props => (props.row ? "row" : "column")};
-  flex-grow: 0;
+  flex-wrap: ${props => (props.wrap ? "wrap" : "nowrap")};
+  justify-content: center;
+  justify-content: flex-start;
+  justify-content: space-around;
+  align-items: center;
+  align-content: space-between;
 `;
 
-export const Flex = ({ children, row }) => (
-  <BoxContainer row={row}>{children}</BoxContainer>
+export const Flex = ({ children, row, flow, flexDirextion, wrap }) => (
+  <BoxContainer row={row} flow={flow} flexDirextion={flexDirextion} wrap={wrap}>
+    {children}
+  </BoxContainer>
 );
